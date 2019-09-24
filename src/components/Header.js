083135f0db5761f67bb78b2e183af16af268a32d
mchoy30 <mchoy30@academic.rrc.ca>
2019-09-23 21:38:20 -0500

    Styled Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,38 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
-export default ({children}) =>{
+const Header = ({children}) => {
+
+  const style = {
+    display: 'flex',
+    position:'flex-row',
+    margin:'20px',
+    marginBottom:'0px',
+    padding:'20px',
+    color:'red'
+  };
+
+  const linkStyle = {
+    textDecoration:'none',
+    borderBottom:'3px solid white',
+    color: "white",
+    fontFamily: 'Segoe UI',
+    aHover: 'blue'
+
+  }
+
+
   return(
-    <div>
-      <div>
-        <h3><Link to='/'>Home</Link></h3>
-        <h3><Link to='/resume'>Resume</Link></h3>
-        <h3><Link to='/projects'>Projects</Link></h3>
+    <div >
+      <div style={{display:'flex', flexDirection: 'row',justifyContent: 'flex-end',backgroundColor:'#0080ff', height:'10vh'}}>
+        <h3 style={style}><NavLink style={linkStyle} to='/'>Home</NavLink></h3>
+        <h3 style={style}><NavLink style={linkStyle} to='/resume'>Resume</NavLink></h3>
+        <h3 style={style}><NavLink style={linkStyle} to='/projects'>Projects</NavLink></h3>
       </div>
       {children}
     </div>
   )
 
 }
+
+export default Header;
