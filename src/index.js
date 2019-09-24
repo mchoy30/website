fb048276137c19a6e60b0c856fdb71b97d5afc52
mchoy30 <mchoy30@academic.rrc.ca>
2019-09-23 20:25:02 -0500

    Added router

--- src/index.js
+++ src/index.js
@@ -10,7 +10,13 @@ import {Router, Switch, Route} from 'react-router-dom';
 
 
 
-ReactDOM.render(<Header><App /></Header>, document.getElementById('root'));
+ReactDOM.render(
+  <Router history={createBrowserHistory()}>
+    <Switch>
+      <Route exact path ='/' render={()=><Header><App /></Header>} />
+    </Switch>
+  </Router>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
