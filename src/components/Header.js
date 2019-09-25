019001837bfb2d520bc48d90046e781e9be2e03e
mchoy30 <mchoy30@academic.rrc.ca>
2019-09-25 18:12:49 -0500

    Removed unused styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,22 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
-
+import Hover from  '../NavLink.css';
 const Header = ({children}) => {
 
   const style = {
     display: 'flex',
     position:'flex-row',
     margin:'20px',
-    marginBottom:'0px',
-    padding:'20px',
-    color:'red'
+    marginBottom:'20px',
+    padding:'10px',
   };
 
-  const linkStyle = {
-    textDecoration:'none',
-    borderBottom:'3px solid white',
-    color: "white",
-    fontFamily: 'Segoe UI',
-    aHover: 'blue'
-
-  }
-
-
   return(
-    <div >
-      <div style={{display:'flex', flexDirection: 'row',justifyContent: 'flex-end',backgroundColor:'#0080ff', height:'10vh'}}>
-        <h3 style={style}><NavLink style={linkStyle} to='/'>Home</NavLink></h3>
-        <h3 style={style}><NavLink style={linkStyle} to='/resume'>Resume</NavLink></h3>
-        <h3 style={style}><NavLink style={linkStyle} to='/projects'>Projects</NavLink></h3>
+    <div style={{ height:'20px', width:'100vw'}}>
+      <div style={{display:'flex', flexDirection: 'row',justifyContent: 'flex-end',backgroundColor:'#0288D1'}}>
+        <h3 style={style}><NavLink activeStyle={Hover} to='/'>About</NavLink></h3>
+        <h3 style={style}><NavLink style={Hover} to='/resume'>Resume</NavLink></h3>
+        <h3 style={style}><NavLink style={Hover} to='/projects'>Projects</NavLink></h3>
       </div>
       {children}
     </div>
