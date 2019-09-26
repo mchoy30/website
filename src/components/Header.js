785ea4c7c460e0686f30f37eeef461584dcabf35
mchoy30 <mchoy30@academic.rrc.ca>
2019-09-26 01:16:16 -0500

    Fixed css style

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,13 @@ import Hover from  '../NavLink.css';
 const Header = ({children}) => {
 
   const style = {
-    display: 'flex',
-    position:'flex-row',
     margin:'20px',
-    marginBottom:'20px',
-    padding:'10px',
+    padding:'0px',
   };
 
   return(
-    <div style={{ height:'20px', width:'100vw'}}>
-      <div style={{display:'flex', flexDirection: 'row',justifyContent: 'flex-end',backgroundColor:'#0288D1'}}>
+    <div style={{position:'', height:'10px', width:'100vw', marginBottom:'20px',maxWidth:'100%'}}>
+      <div style={{display:'flex', flexDirection:'row',justifyContent: 'flex-end',backgroundColor:'#0288D1'}}>
         <h3 style={style}><NavLink activeStyle={Hover} to='/'>About</NavLink></h3>
         <h3 style={style}><NavLink style={Hover} to='/resume'>Resume</NavLink></h3>
         <h3 style={style}><NavLink style={Hover} to='/projects'>Projects</NavLink></h3>
