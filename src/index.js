a12980f5b9999359a14aa805158cf02b2ef1915e
mchoy30 <mchoy30@academic.rrc.ca>
2019-09-28 11:56:28 -0500

    Added Hashrouter

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Projects from './components/Projects';
 import Header from './components/Header';
 import Resume from './components/Resume';
 import { createBrowserHistory } from "history";
-import {Router, Switch, Route} from 'react-router-dom';
-
-
+import {Router, Switch, Route, HashRouter} from 'react-router-dom';
+import About from './components/About';
 
 
 ReactDOM.render(
-  <Router history={createBrowserHistory()}>
+  <HashRouter basename='/' history={createBrowserHistory()}>
 
     <Switch>
-      <Route exact path ='/' render={()=><Header><App /></Header>} />
+      <Route exact path ='/' render={()=><Header><About /></Header>} />
       <Route path ='/resume' render={()=><Header><Resume /></Header>} />
+      <Route path ='/projects' render={()=><Header><Projects /></Header>} />
     </Switch>
-  </Router>, document.getElementById('root')
+  </HashRouter>, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
