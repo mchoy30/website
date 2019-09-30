9070b2032817c1d3f499b61e75505b5846dc1b53
mchoy30 <mchoy30@academic.rrc.ca>
2019-09-30 02:53:37 -0500

    Changed bio paragraph

--- src/components/About.js
+++ src/components/About.js
@@ -39,8 +39,8 @@ export default function(){
                       }}>
                 <h1>Experience</h1>
                 <p>
-                    I have experience in several languages through Red river and their coding projects such as a C# banking application that focused on learning design patterns such as singleton and the observer pattern. <br/>
-                    Experience with PHP scripting for login authentication ensuring properly encrypted passwords and sanitization to mitigate SQL injection attacks.
+                    I have experience in several languages through Red river and their coding projects such as a C# banking application that used a MVC framwork as well as learning design patterns such as singleton and the observer pattern. <br/>
+                    Experience with PHP scripting for login authentication ensuring properly encrypted passwords and sanitization to mitigate SQL injection attacks. <br/>
                     Experience with Ruby on Rails web framework building a full stack E-commerce website using PostgresSQL<br/>
                     I believe some of the best ways to learn is by doing. I haved decided to teach myself React and the more I learn the more I discover. Every time I learn discover more tools and ideas to use!
                 </p>
@@ -58,7 +58,7 @@ export default function(){
               <div>
                   <h1>About me!</h1>
                   {showBio ? <p>
-                    I am a 24 year old BIT graduate born I have tons of hobbies that I love! One of my biggest interests would have to be music. I have always been someone who will try to learn something by themselves through trial and error. At the age of 15 I started to learn how to play guitar to get girls . . . well that didn’t work but I still play guitar to this day and have been recently learning more about musical theory to understand why the things I play work. Another one of my big passions is cooking. I
+                    I am a 24 year old BIT graduate born I have tons of hobbies that I love! One of my biggest interests would have to be music. I have always been someone who will try to learn something by themselves through trial and error. At the age of 15 I started to learn how to play guitar to get girls . . . well that didn’t work but I still play guitar to this day and have been recently learning more about musical theory to understand why the things I play work. I am always up to the challenge of learning new things
                   </p> : <p>I am a 24 year old BIT graduate born I have tons of hobbies that I love! One of my biggest interests would have to be music.</p>}
               </div>
               <Button style={{display:'flex', alignItems:'center', color:"#FFEB3B"}}
