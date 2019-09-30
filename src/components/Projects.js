843b0087bb04fb94bd2609d022b1ed7694af40f5
mchoy30 <mchoy30@academic.rrc.ca>
2019-09-30 02:07:25 -0500

    Fixed styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,12 +9,10 @@ export default () =>{
   }
 
   return(
-      <div style={{backgroundColor:'#03A9F4',position:'relative',height:'100vh', display:'flex', flexDirection:'column',fontFamily:'Segoe UI',}}>
+      <div style={{backgroundColor:'#03A9F4',position:'relative',height:'100vh',display:'flex', flexDirection:{},fontFamily:'Segoe UI',overflow:'hidden', width:'100vw'}}>
 
-          <div className="slide-in-right"  style={{ top:'0px', zIndex:'2'}}>
-            <Phone />
-          </div>
-          <div className="slide-in-bottom" style={{width:'700px',marginTop:'50px',marginBottom:'80px', zIndex:'3', position:'absolute', left:'180px', top:'20px'}}>
+
+          <div className="slide-in-bottom" style={{width:'700px',marginTop:'50px',marginBottom:'80px',marginLeft:'200px', zIndex:'3', position:'flex', top:'20px', }}>
               <div style={style}>
                 <h1>Current Project<span style={{color:'#FFEB3B', fontSize:'12'}}> .</span></h1>
                 <p>The List is a list app for keeping track of my friend Kat and I have plans that we want to do but together so we would always say we would” Add it to the list!”. This was a project I decided to do to help me learn the React JavaScript framework. Along with helping me learn React I have started to learn about other technologies that I had not used before such as Node.JS with Express and learn how to use MongoDb. The current version of this app is hosted on Heroku with future plans and projects in store!</p>
@@ -31,7 +29,11 @@ export default () =>{
               </div>
           </div>
 
-
+          <div style={{display:'flex', width:'40%'}}>
+            <div className="slide-in-right"  style={{zIndex:'2',disply:'flex',alignContent:'flex-start',  right:'200px',}}>
+              <Phone />
+            </div>
+          </div>
       </div>
   )
 
