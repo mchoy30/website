4d2da3f17686dd11c56e10d3cf671dd45df3fe97
mchoy30 <mchoy30@academic.rrc.ca>
2019-09-30 20:23:46 -0500

    Added social media data

---
+++ src/data/soscialProfiles.js
@@ -0,0 +1,19 @@
+import gitHubIcon from '../assets/github_icon.png';
+import emailIcon from '../assets/email_icon.png';
+
+
+const SOCIAL_PROFILES = [
+  {
+    id: 1,
+    link: 'mailto:mackenziechoy@gmail.com',
+    image: emailIcon
+  },
+  {
+    id:2,
+    link: 'https://github.com/mchoy30',
+    image: gitHubIcon
+  }
+
+];
+
+export default SOCIAL_PROFILES;
