c543075b4cd545ec0d301287e9e4477602b9aa82
mchoy30 <mchoy30@academic.rrc.ca>
2019-10-09 17:33:09 -0500

    Added responsive support

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,16 @@ import ScreenContext from '../Context/ScreenContext';
 import {NavLink} from 'react-router-dom';
 import Hover from  '../NavLink.css';
 const Header = () => {
-  const screenSize = useContext(ScreenContext);
   const style = {
     margin:'20px',
   };
-
+  const screenSize = useContext(ScreenContext);
   return(
     <div style={{marginBottom:'0px', postition:'fixed'}}>
 
       <div style={{display:'flex', flexDirection:'row',justifyContent: 'flex-end',backgroundColor: screenSize.medium ? '#0288D1' : '#0288D1'}}>
-        <div style={{display:'flex',color:'#fff',alignItems: 'center',justifyContent: 'center', fontFamily:'Satisfy, cursive',width:'50%',position:'relative',right:'166px',}}>
-          <NavLink to='/' style={{fontSize:'35px',display:'flex',fontFamily:'Satisfy, cursive'}}  > Mackenzie Choy</NavLink>
+        <div style={{display:'flex',color:'#fff',alignItems:screenSize.large ? 'flex-end' :'center',justifyContent: 'center', fontFamily:'Satisfy, cursive',width:'50%',position:'relative', right:screenSize.large ? '': '166px',}}>
+          <NavLink to='/' style={{fontSize:'35px',display:'flex',fontFamily:'Satisfy, cursive',}}  > {screenSize.large ? 'Mchoy': 'Mackenzie Choy' } </NavLink>
         </div>
 
         <h3 className="links" style={style}><NavLink activeStyle={Hover} to='/'>About</NavLink></h3>
