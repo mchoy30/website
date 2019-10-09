e5d8e01b136577c0864d40558d6a33867ad814ba
mchoy30 <mchoy30@academic.rrc.ca>
2019-10-09 17:34:28 -0500

    Added showcase data

---
+++ src/data/showcases.js
@@ -0,0 +1,30 @@
+import ES6 from '../assets/ES6.jpg';
+import JS from '../assets/JSWeird.jpg';
+import ReactBootCamp from '../assets/ReactBootCamp.jpg';
+
+const SHOW_CASES = [
+  {
+    id: 1,
+    title: 'The Full JavaScript & ES6 Tutorial - (including ES7 & React)',
+    image: ES6,
+    description: 'The most in-depth course on ES6 around. Start with JavaScript, deep-dive into ES6, & even more (Webpack, ES7, & React)!',
+    link: 'https://www.udemy.com/share/101ArABUQTdlpSR34=/'
+
+  },
+  {
+    id: 2,
+    title: 'JavaScript: Understanding the Weird Parts',
+    image: JS,
+    description: 'An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+    link: 'https://www.udemy.com/share/101XjUBUQTdlpSR34=/'
+  },
+  {
+    id: 3,
+    title: 'React JS Web Development - The Essentials Bootcamp',
+    image: ReactBootCamp,
+    description: 'Bootcamp on the React.js essentials. Gain a strong foundation of the core concepts, and build exciting and useful apps!',
+    link: 'https://www.udemy.com/share/101v3eBUQTdlpSR34=/'
+  }
+]
+
+export default SHOW_CASES;
